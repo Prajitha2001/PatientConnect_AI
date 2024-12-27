14f494e9ecac58c2cdf2efedec2f9855b78a89a4
Prajitha2001 <prajithak980@gmail.com>
2024-12-27 16:50:45 +0530

    backend.py

--- backend.py
+++ backend.py
@@ -65,4 +65,5 @@ def response():
         return jsonify({"error": str(e)}), 500
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))  # Use PORT from environment or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
